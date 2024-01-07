f6ed34012a8668e8cb68c2896149a89c80caf626
Suphi Erkin Karaçay <serkinkaracay@gmail.com>
2024-01-08 02:21:53 +0300

    ACC and ADC lists added

--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -1,56 +1,91 @@
-/* eslint-disable @next/next/no-img-element */
 'use client';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'primereact/button';
-import { Column } from 'primereact/column';
-import { DataTable } from 'primereact/datatable';
-import { Menu } from 'primereact/menu';
-import React, { useContext, useEffect, useRef, useState } from 'react';
-import { ProductService } from '../../demo/service/ProductService';
-import { LayoutContext } from '../../layout/context/layoutcontext';
-import { Demo } from '@/types';
+import { DataView } from 'primereact/dataview';
+import axios from 'axios';
 
-
-
-const Dashboard = () => {
-    const [products, setProducts] = useState<Demo.Product[]>([]);
-    const menu1 = useRef<Menu>(null);
-    const menu2 = useRef<Menu>(null);
-    const { layoutConfig } = useContext(LayoutContext);
+function Dashboard() {
+    const [accData, setAccData] = useState([]);
+    const [adcData, setAdcData] = useState([]);
 
     useEffect(() => {
-        ProductService.getProductsSmall().then((data) => setProducts(data));
+        axios.get("http://localhost:8000/api/acc/")
+            .then(res => {
+                setAccData(res.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+
+        axios.get("http://localhost:8000/api/adc/")
+            .then(res => {
+                setAdcData(res.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
     }, []);
 
-    
-    const formatCurrency = (value: number) => {
-        return value?.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD'
-        });
+    const accItemTemplate = (accData) => {
+        return (
+            <div className="col-12">
+                <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+                <img src={"/layout/images/ACC.png"} width="90px" height="90px" alt={accData.name} />
+                    <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                        <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+                            <div className="text-2xl font-bold text-900">{accData.name}</div>
+                            <span>{accData.address}</span>
+                            <div className="flex align-items-center gap-3">
+                                <span className="flex align-items-center gap-2">
+                                    <i className="pi pi-map-marker"></i>
+                                    <span className="font-semibold">{accData.location}</span>
+                                </span>
+                            </div>
+                        </div>
+                        {/*<div className="flex align-items-center sm:align-items-end gap-3 sm:gap-2">
+                            <i className="pi pi-info-circle my-auto"></i>
+                            <Button className="p-button-rounded">Details</Button>
+                        </div>*/}
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    const adcItemTemplate = (adcData) => {
+        return (
+            <div className="col-12">
+                <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+                    <img src={"/layout/images/ADC.png"} width="90px" height="90px" alt={adcData.name} />
+                    <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+                        <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+                            <div className="text-2xl font-bold text-900">{adcData.name}</div>
+                            <span>{adcData.address}</span>
+                            <div className="flex align-items-center gap-3">
+                                <span className="flex align-items-center gap-2">
+                                    <i className="pi pi-map-marker"></i>
+                                    <span className="font-semibold">{adcData.location}</span>
+                                </span>
+                            </div>
+                        </div>
+                        {/*<div className="flex align-items-center sm:align-items-end gap-3 sm:gap-2">
+                            <i className="pi pi-info-circle my-auto"></i>
+                            <Button className="p-button-rounded">Details</Button>
+                        </div>*/}
+                    </div>
+                </div>
+            </div>
+        );
     };
 
     return (
-        <div className="grid">
-            <div className="col-12">
-                <div className="card">
-                    <h5>Recent Sales</h5>
-                    <DataTable value={products} rows={5} paginator responsiveLayout="scroll">
-                        <Column header="Image" body={(data) => <img className="shadow-2" src={`/demo/images/product/${data.image}`} alt={data.image} width="100" />} />
-                        <Column field="name" header="Name" sortable style={{ width: '35%' }} />
-                        <Column field="price" header="Price" sortable style={{ width: '35%' }} body={(data) => formatCurrency(data.price)} />
-                        <Column
-                            header="View"
-                            style={{ width: '100%' }}
-                            body={() => (
-                                <>
-                                    <Button icon="pi pi-search" text />
-                                </>
-                            )}
-                        />
-                    </DataTable>
-                </div>
-            </div>
-        </div>
+        <>
+            <h2>All ACCs</h2>
+            <DataView value={accData} itemTemplate={accItemTemplate} paginator rows={5} />
+            <br />
+            <h2>All ADCs</h2>
+            <DataView value={adcData} itemTemplate={adcItemTemplate} paginator rows={5} />      
+        </>
     );
 };
 
