c6282422efa10b048b7585401848632a32937697
Nihat Özdemir <nihat20ozdemir00@gmail.com>
2024-04-19 11:45:48 +0300

    duplication second fix

--- backend/backend/aid/views.py
+++ backend/backend/aid/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework import status
 
 
 from backend.aid.models import ACCAid, ADCAid, ACC, ADC, EMA, AidType, AidTypeRequest
